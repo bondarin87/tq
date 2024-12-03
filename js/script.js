daa38c6b29b19f6efca1efb22a8c9eb264d93717
bondarin87 <110008441+bondarin87@users.noreply.github.com>
2024-12-03 18:56:05 -0500

    Actualizar script.js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,6 @@
 let body1 = document.querySelector('body');
 let tapa = document.querySelector('.tapa');
-body1.style.backgroundImage = 'url(/pics/tq.jpeg)';
-body1.style.backgroundRepeat = 'repeat';
-body1.style.backgroundSize = 'cover';
+
 let i = 3;
 setInterval(() => 
 	tapa.textContent = i--, 
