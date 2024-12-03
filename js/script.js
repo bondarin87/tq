8b0e48954df1effaea8ae5a3bfed6e9d2ae6529c
bondarin87 <110008441+bondarin87@users.noreply.github.com>
2024-12-03 18:17:34 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+let body1 = document.querySelector('body');
+let tapa = document.querySelector('.tapa');
+body1.style.backgroundImage = 'url(/pics/tq.jpeg)';
+body1.style.backgroundRepeat = 'repeat';
+body1.style.backgroundSize = 'cover';
+let i = 3;
+setInterval(() => 
+	tapa.textContent = i--, 
+	1000);
+   
+setTimeout(function() {
+   tapa.style.display = 'none';
+} , 4000);
